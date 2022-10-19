a87840b0f9c2f816ea76fee91048c7b9d74b797d
Igoryani4 <110982221+Igoryani4@users.noreply.github.com>
2022-10-19 12:47:22 +0300

    Update Task_3.py

--- Task_3.py
+++ Task_3.py
@@ -4,3 +4,18 @@
 # 47756688399943 -> [5]
 # 1113384455229 -> [8,9]
 # 1115566773322 -> []
+
+
+print ('Это программа, которая выведет список неповторяющихся элементов исходной последовательности')
+num = input('Введите число для проверки: ')
+result = []
+count = 0 
+for i in range (len(num)):
+    for j in range (len(num)):
+        if num[j] == num [i] :
+            count += 1
+    if count == 1:
+        result.append(num[i])
+    count = 0
+    
+print (result)
