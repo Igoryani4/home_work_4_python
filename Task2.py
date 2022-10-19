dc0b4606bb001874434e0cabd463dfbf6c9b522c
Igoryani4 <110982221+Igoryani4@users.noreply.github.com>
2022-10-19 13:34:44 +0300

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -4,12 +4,21 @@ print ('Это программа, которая которая состави�
 num = int (input('Введите число: '))
 num_2 = num
 i = 2
+count = 0
+flag = False
 div = []
 while num_2 != 1:
-    if num_2 % i == 0:
+    for k in range(1, i+1):
+        if i % k == 0:
+            count += 1 
+    if count == 2:
+        flag = True
+        count = 0   
+        
+    if num_2 % i == 0 and flag == True:
         num_2 /= i 
         div.append(str(i))
-        
+        flag = False
     else:
         i += 1
 print(f'Простые делители для числа {num} =','*'.join(div))
