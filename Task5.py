02999606a892de6b70471e34fd7a10c3b61611c2
Igoryani4 <110982221+Igoryani4@users.noreply.github.com>
2022-10-20 13:54:11 +0300

    Update Task5.py

--- Task5.py
+++ Task5.py
@@ -1,3 +1,8 @@
 list1 = '23x^9 - 16x^8 + 3x^7 + 15x^4 - 2x^3 + x^2 + 20 = 0'
-lict2 = '17x^9 + 15x^8 - 8x^7 + 15x^6 - 10x^4 + 7x^3 - 13x + 33 = 0'
+list2 = '17x^9 + 15x^8 - 8x^7 + 15x^6 - 10x^4 + 7x^3 - 13x + 33 = 0'
 
+list1_1 = list1.replace(' ', '')[: -2]
+list2_1 = list2.replace(' ', '')[: -2]
+
+list1_2 = list1_1.replace('-', '+-').split('+')
+list2_2 = list1_1.replace('-', '+-').split('+')
