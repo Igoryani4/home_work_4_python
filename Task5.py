1738016e3ada61d715ae56ae46cc98ecb0922726
Igoryani4 <110982221+Igoryani4@users.noreply.github.com>
2022-10-20 18:09:21 +0300

    Update Task5.py

--- Task5.py
+++ Task5.py
@@ -1,5 +1,9 @@
-list1 = '23x^9 - 16x^8 + 3x^7 + 15x^4 - 2x^3 + x^2 + 20 = 0'
-list2 = '17x^9 + 15x^8 - 8x^7 + 15x^6 - 10x^4 + 7x^3 - 13x + 33 = 0'
+with open('t1.txt', 'r') as f:
+   list1= f.readline()
+   
+with open('t2.txt', 'r') as f:
+   list2= f.readline()
+
 
 list1_1 = list1.replace(' ', '')[: -2]
 list2_1 = list2.replace(' ', '')[: -2]
